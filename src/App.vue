6488c7d15642cb41bd23984517edea61ebcd05ad
Gung9527 <1403245074@qq.com>
2022-05-13 21:05:02 +0800

    完善grid组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <!-- <v3-bar
+  <v3-bar
     :data="barData"
     :metrics-alias="metricsAlias"
     :tooltip-setting="tooltipSetting"
     :y-axis-setting="yAxisSetting"
+    :y-axis-type="'category'"
+    :x-axis-type="'value'"
     :series-settings="seriesSettings"
     tooltip-trigger="axis"
-  ></v3-bar> -->
-  <v3-grid>
+  ></v3-bar>
+  <!-- <v3-grid>
     <v3-bar
       :data="barData"
       :metrics-alias="metricsAlias"
     ></v3-bar>
-  </v3-grid>
+  </v3-grid> -->
 </template>
 
 <script setup lang="ts">
@@ -23,14 +25,14 @@ import { ref } from 'vue'
 
 
 const barData: Data = {
-  columns: ['year', 'value'],
+  columns: ['year', 'value', 'value1'],
   rows: [
-    { year: '2013', value: 244423.85 },
-    { year: '2014', value: 245011.15 },
-    { year: '2015', value: 245624.16 },
-    { year: '2016', value: 246233.85 },
-    { year: '2017', value: 247314.02 },
-    { year: '2018', value: 248681.97 },
+    { year: '2013', value: 244423.85, value1: 10000 },
+    { year: '2014', value: 245011.15, value1: 10000 },
+    { year: '2015', value: 245624.16, value1: 10000 },
+    { year: '2016', value: 246233.85, value1: 10000 },
+    { year: '2017', value: 247314.02, value1: 10000 },
+    { year: '2018', value: 248681.97, value1: 10000 },
   ]
 }
 
